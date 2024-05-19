b8a892bca39626e025a763801687d52c8bc64ba4
kevinsenan <125194484+kevinsenan@users.noreply.github.com>
2024-05-19 14:04:43 +0100

    Update dbconfig.py

--- dbconfig.py
+++ dbconfig.py
@@ -1,7 +1,7 @@
 
 mysql = {
-    'host':"localhost",
-    'user':"",
-    'password':"",
-    'database':""
+    'host':"KevDon.mysql.pythonanywhere-services.com",
+    'user':"KevDon",
+    'password':"Mfg13579!@",
+    'database':"KevDon$datarepresentation"
 }
