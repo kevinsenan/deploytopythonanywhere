2f992796f666114a2a75fd6b27311be6b9aff4eb
kevinsenan <kevinsenan@hotmail.com>
2024-05-20 15:59:37 +0100

    took out dbase log details

--- dbconfig.py
+++ dbconfig.py
@@ -1,7 +1,7 @@
 
 mysql = {
-    'host':"KevDon.mysql.pythonanywhere-services.com",
-    'user':"KevDon",
-    'password':"Mfg13579!@",
-    'database':"KevDon$datarepresentation"
+    'host':"",
+    'user':"",
+    'password':"",
+    'database':""
 }
